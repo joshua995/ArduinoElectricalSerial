6b764082c957bf38396eec5f70dbff0d95c78a71
joshua995 <114888100+joshua995@users.noreply.github.com>
2023-07-26 18:48:47 -0600

    Add files via upload

--- ElectricalArduino.ino
+++ ElectricalArduino.ino
@@ -1,23 +1,36 @@
 /*Joshua Liu
 Arduino - Python Electrical
 */
-#define photoresistor A0
+#define photoresistor A0 //Plug in the photoresitor in pin A0
+#define thermistor A1 //Plug in the thermistor in pin A1
 
 String message = "";
 int brightness = 0;
-int lights = 3;
+int lights = 3; //Digital PWM pin
+
+int tempResistance = 0;
+int fanSpeed = 0;
+int fans = 4; //Digital PWN pin
 
 bool isDark = false;
 
 void setup() {
   Serial.begin(9600);
   pinMode(photoresistor, INPUT);
+  pinMode(thermistor, INPUT);
   pinMode(lights, OUTPUT);
+  pinMode(fans, OUTPUT);
   digitalWrite(lights, LOW);
 }
 
 void loop() {
   Serial.println("Photoresistor: " + String(analogRead(photoresistor)));
+
+  tempResistance = analogRead(thermistor);
+  fanSpeed = map(tempResistance, 0, 900, 255, 0);
+  //Serial.print("fan speed: "); Serial.println(fanSpeed);
+  analogWrite(fans, fanSpeed);
+  delay(1000);
   if(analogRead(photoresistor) <= 50){
     if (!isDark){
       digitalWrite(lights, HIGH);
